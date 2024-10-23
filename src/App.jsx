6bee44155d0998eacb094fe6e52250f7ad3347c5
Badalsingh2 <Badalubentu@gmail.com>
2024-10-23 23:33:08 +0530

    added gameover and changed direction of asteroid

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import Hero from "./components/Hero"
 
 const App = () => {
@@ -5,7 +6,10 @@ const App = () => {
     <div>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <Hero />
+      
     </div>
+    
+    
   )
 }
 
