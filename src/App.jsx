a67d3f07665991276adcf07ee87623cd4331aacc
PratyayKoley <pratyaykoley@gmail.com>
2024-10-24 17:22:24 +0530

    Planets

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Spaceship_Model from "./components/Spaceship_Model"
 import Earth from "./components/Earth"
 import Mars from "./components/Mars"
 import Pluto from "./components/Pluto"
+import Jupiter from "./components/Jupiter"
+import Neptune from "./components/Neptune"
+import Uranus from "./components/Uranus"
+import Saturn from "./components/Saturn"
 
 const App = () => {
   return (
     <div>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      <Pluto />
+      <Saturn />
       
     </div>
     
