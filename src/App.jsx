541c1320b4e9996d9083ea9da6562e1550e1f0e1
PratyayKoley <pratyaykoley@gmail.com>
2024-10-24 18:37:57 +0530

    Frontend Routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,32 @@ import Jupiter from "./components/Jupiter"
 import Neptune from "./components/Neptune"
 import Uranus from "./components/Uranus"
 import Saturn from "./components/Saturn"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Leaderboard from "./components/Leaderboard"
+import Store from "./components/Store"
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Hero />
+    },
+    {
+      path: "/leaderboard",
+      element: <Leaderboard />
+    },
+    {
+      path: "/store",
+      element: <Store />
+    }
+  ])
+
   return (
-    <div>
+    <>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      <Saturn />
-      
-    </div>
-    
-    
+      <RouterProvider router={router} />
+    </>
+
   )
 }
 
