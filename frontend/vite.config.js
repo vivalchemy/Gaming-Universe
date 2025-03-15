9f39a71bdf792c46b3af68ab4a829492a1e4086d
Vivian Ludrick <116781909+vivalchemy@users.noreply.github.com>
2025-03-15 13:58:12 +0530

    owie

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://gaming-universe-1.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
